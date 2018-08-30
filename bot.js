e90e15075bffd1a378312ba236ebbe93300447a5
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-08-30 13:54:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -43,7 +43,6 @@ client.on('message', msg => {
   }
 });
 
-const prefix = '$'
 
 
 
