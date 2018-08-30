a4a05444d53528553b1c170b828eb03965e38c65
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-08-30 14:52:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,50 +147,7 @@ client.on("guildMemberAdd", member => {
 
 
 
-client.on('message', message => {
 
-
-           if (message.content.startsWith(prefix + "id")) {
-
-                message.guild.fetchInvites().then(invs => {
-      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-      var moment = require('moment');
-      var args = message.content.split(" ").slice(1);
-let user = message.mentions.users.first();
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-        moment.locale('ar-TN');
-      var id = new  Discord.RichEmbed()
-
-    .setColor("#0a0909")
- .setThumbnail(message.author.avatarURL)
-.addField(': Date of join to discord',\${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} `\n ${moment(heg.createdTimestamp).fromNow()},true) 
-.addField(': Date of join to the server',`${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')}  ` \n ${moment(h.joinedAt).fromNow()} , true)
-.addField( :Invite by , ${inviteCount} `)
-
-
-.setFooter(message.author.username, message.author.avatarURL)
-    message.channel.sendEmbed(id);
-})
-}
-
-
-
-     });
 
 
 
