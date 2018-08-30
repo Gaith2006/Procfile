4ff54b8238fe47ed8ff6423c3cb286b1ab6fedf7
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-08-30 13:53:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,33 +30,20 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const prefix = '$'
-
-client.on('message', message =>{
-    let args = message.content.split(' ');
-    let prefix = '$'; 
-    
-    if(args[0] === `${prefix}avatar`){
-        let mentions = message.mentions.members.first()
-        if(!mentions) {
-          let sicon = message.author.avatarURL
-          let embed = new Discord.RichEmbed()
-          .setImage(message.author.avatarURL)
-          .setColor("#f7abab") 
-          .setDescription(`**${message.author.username}#${message.author.discriminator}**'s avatar :`);
-          message.channel.send({embed})
-        } else {
-          let sicon = mentions.user.avatarURL
-          let embed = new Discord.RichEmbed()
-          .setColor("#f7abab")
-          .setDescription(`**${mentions.user.username}#${mentions.user.discriminator}**'s avatar :`)
-          .setImage(sicon)
-          message.channel.send({embed})
-        }
-    };
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+  if (msg.content === '$ping') {
+    msg.channel.send('Pong!');
+  }
+});
+
+const prefix = '$'
 
 
 
