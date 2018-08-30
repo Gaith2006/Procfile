912b93f6a4beb1999198802f70457b8fb3b1463f
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-08-30 23:21:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1120,7 +1120,7 @@ function getValue(key, array) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "Speed Bot";
+    let copy = "Zindawrs";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
@@ -2534,7 +2534,9 @@ client.on('message', message => {
 
 
 
-client.login(process.env.BOT_TAG)}
+client.login(process.env.BOT_TOKEN)
+
+
 	
 	
 	
