d80603265d0285e8ab15368fec82fb548b3e9a80
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-08-30 17:17:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2286,8 +2286,8 @@ client.on('message', message => {
             if(!message.channel.guild) return;
         let embed = new Discord.RichEmbed()
         .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
-        .setTitle(`:small_orange_diamond: اضغط هنا `)
-        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=484098782158651392&permissions=8&scope=bot%27`)
+        .setTitle(`:small_orange_diamond: Click Here `)
+        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=484098782158651392&permissions=8&scope=bot`)
         .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
      message.channel.sendEmbed(embed);
        }
