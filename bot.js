9bac7f0ce513dfc9f39842967808daea28cc69af
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-08-30 14:28:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,7 +137,13 @@ client.on('message', message => {
 
 
 
-
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
 
 
 
