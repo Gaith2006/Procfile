ac8e6f89130379fc44692ac6ebabdbc0915f87f9
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-08-30 13:37:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,5 +28,197 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+
+
+
+const prefix = '$'
+
+  
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+
+
+client.on('message', message =>{
+    let args = message.content.split(' ');
+    let prefix = '$'; 
+    
+    if(args[0] === `${prefix}avatar`){
+        let mentions = message.mentions.members.first()
+        if(!mentions) {
+          let sicon = message.author.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setImage(message.author.avatarURL)
+          .setColor("#f7abab") 
+          .setDescription(`**${message.author.username}#${message.author.discriminator}**'s avatar :`);
+          message.channel.send({embed})
+        } else {
+          let sicon = mentions.user.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setColor("#f7abab")
+          .setDescription(`**${mentions.user.username}#${mentions.user.discriminator}**'s avatar :`)
+          .setImage(sicon)
+          message.channel.send({embed})
+        }
+    };
+});
+
+
+
+
+client.on('message', message => {
+     if(message.content.startsWith(prefix + "clear")) {
+         var args = message.content.split(" ").slice(1);
+ if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You need MANAGE_MESSAGES permission noob');
+  if (!args[0]) return message.channel.send('You didn\'t provide any number!!!');
+
+  message.channel.bulkDelete(args[0]).then(() => {
+    const embed = new Discord.RichEmbed()
+      .setColor(0xF16104)
+      .setDescription(`Cleared ${args[0]} messages.`);
+    message.channel.send({ embed });
+
+    const actionlog = message.guild.channels.find('name', 'logs');
+
+    if (!actionlog) return message.channel.send('Can\'t find action-log channel. Are you sure that this channel exists and I have permission to view it? **CANNOT POST LOG.**');
+    const embedlog = new Discord.RichEmbed()
+      .setDescription('~Purge~')
+      .setColor(0xF16104)
+      .addField('Purged By', `<@${message.author.id}> with ID ${message.author.id}`)
+      .addField('Purged in', message.channel)
+      .addField('Time', message.createdAt);
+    actionlog.send(embedlog);
+   
+  });
+};
+
+});
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === prefix + "help-admin") {
+         if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply(`
+         
+         **لست من المشرفين لن يتم ارسال الرسالة إليك**
+         
+         
+         `);
+         message.channel.send('**Check your DM**');
+            
+    
+         
+
+
+ message.author.sendMessage(`
+ **
+
+=============================
+       ADMIN S
+=============================
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  $kick <ntion 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  $clear 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+  $mute mention 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  $unmute mention
+ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+  $bc message
+ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  $closero<message
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+  $openrom<message
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  $server message
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  $wran massage
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  $clear message
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+`);
+
+    }
+});
+
+
+
+client.on('message' , message => {
+  var prefix = "$";
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "ping")) {
+ message.channel.send('Pong...').then((msg) => {
+      msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nDiscord API: ${Math.round(client.ping)} ms.\`\`\``);
+ })
+  }  
+ });
+
+
+client.on('message',function(message) {
+  if (message.author.bot) return;
+
+
+                  if(!message.channel.guild) return;
+
+                    if (message.content === prefix + "members") {
+ const embed = new Discord.RichEmbed()
+
+    .setDescription(`**Members info 
+ ONLINE:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+  NOT DISTURB       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+  IDLE:     ${message.guild.members.filter(m=>m.presence.status == 'idle').size}
+  OFLINE:     ${message.guild.members.filter(m=>m.presence.status == 'offline').size}
+   MEMBERS:  ${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size}
+ BOTS: ${message.guild.members.filter(m=>m.user.bot).size} **`)
+         message.channel.send({embed});
+
+    }
+      });
+
+
+
+
+client.on('message', message => {
+   if (message.content === "$id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" +  message.author.discriminator, true)
+  .addField("ID:", message.author.id, true)
+  .addField("Create At:", message.author.createdAt, true)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TAG);
 
