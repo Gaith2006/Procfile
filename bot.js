9b7b16e71c2f45fd7ae1188afdfbd516c1bec167
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-09-04 21:12:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2575,26 +2575,7 @@ client.on('message', async message => {
 
 
 
- client.on('guildMemberAdd', member => {
-  member.addRole('name', "• Members")
-});
-
-client.on('message', message => {
-  let log = message.guild.channels.find('name', "logs") 
-  let act = message.guild.roles.find('name', "• Verified")
-  let user = message.mentions.members.first();
-  if(message.content.startsWith(prefix + "act")){
-    var embed = new Discord.RichEmbed() 
-    .setAuthor(message.author.username) 
-    .setThumbnail(user.avatarURL)
-    .addField('By', <@${message.author.id}>)
-    .setTimestamp()
-    .setFooter("Codescopyright")
-  log.send({embed})
-  message.channel.send({embed})
-  user.addRole(${act})
-  }
-});
+ 
 
 
 
